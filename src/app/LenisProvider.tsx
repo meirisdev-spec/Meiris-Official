a79b9e537eeb75565545c9afc14b84ae9d5fed5a
Hank Emmanuel Nixon <hankenixon456@gmail.com>
2026-07-18 11:26:05 +0400

    Fixed Bug where pages do not appear on top when going to another

--- src/app/LenisProvider.tsx
+++ src/app/LenisProvider.tsx
@@ -1,10 +1,28 @@
 "use client";
 
-import { ReactLenis } from "lenis/react";
+import { ReactLenis, useLenis } from "lenis/react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+function ScrollRestoration() {
+  const pathname = usePathname();
+  const lenis = useLenis();
+
+  useEffect(() => {
+    if (lenis) {
+      lenis.scrollTo(0, { immediate: true });
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, lenis]);
+
+  return null;
+}
 
 export function LenisProvider({ children }: { children: React.ReactNode }) {
   return (
     <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
+      <ScrollRestoration />
       {children}
     </ReactLenis>
   );
